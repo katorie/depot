fd2cf726ea97c4c2481aa23a34d61eec143a6009
katorie <katoriexxxkatorie@gmail.com>
2014-02-27 07:31:22 +0900

    modify test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,3 +35,11 @@ Product.create(title: 'Rails Test Prescriptions',
   description: 
     %{<p>
         <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 34.95)
