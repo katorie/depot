16dec7d495ddf3c23399b95187842d519d021c64
katorie <katoriexxxkatorie@gmail.com>
2014-03-18 07:31:57 +0900

    relate product to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
+  has_many :line_itema
+
+  before_destroy :ensure_not_referenced_by_my_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -8,4 +12,16 @@ class Product < ActiveRecord::Base
     message:'はGIF、JPG、PNG画像のURLでなければなりません。'
  }
   attr_accessible :title, :price, :description, :image_url  # Rails 3.2で必要
+
+  private
+
+    # この商品を参照している品目がないことを確認する
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, '品目が存在します')
+        return false
+      end
+    end
 end
