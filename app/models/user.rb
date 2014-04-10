b682bf8306aeb0a1ffc091f98d5281f0c05782b3
katorie <katoriexxxkatorie@gmail.com>
2014-04-10 23:33:17 +0900

    add side bar and admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+# encodhing: utf-8
 class User < ActiveRecord::Base
   attr_accessible :name, :password_digest, :password, :password_confirmation
 
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "最後のユーザは削除できません"
+      end
+    end
 end
